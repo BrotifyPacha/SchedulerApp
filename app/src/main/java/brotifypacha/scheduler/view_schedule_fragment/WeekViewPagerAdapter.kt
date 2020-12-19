21583bd88b80e1e126a03653cd50da4ffdc58b29
BrotifyPacha <brotifypacha@gmail.com>
2020-12-19 13:29:40 +0300

    Switch to FragmentStatePagerAdapter for better perfomance

--- app/src/main/java/brotifypacha/scheduler/view_schedule_fragment/WeekViewPagerAdapter.kt
+++ app/src/main/java/brotifypacha/scheduler/view_schedule_fragment/WeekViewPagerAdapter.kt
@@ -4,8 +4,9 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 
-class WeekViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+class WeekViewPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getCount(): Int {
         return 7
