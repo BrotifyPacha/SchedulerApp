7d78a13c54c80e4941485b1f28e36ccbbb4b316e
BrotifyPacha <brotifypacha@gmail.com>
2020-12-30 21:37:08 +0300

    Disable shrinkOnScrollBehavior of FAB in ShceduleListFragment.kt
    Disabled shrinkOnScrollBehavior for better ux and added EndSpacing
    viewType to schedule list recycler view to avoid some items be obscured
    from view by bottomAppBar.

--- app/src/main/java/brotifypacha/scheduler/schedules_list_fragment/ScheduleListAdapter.kt
+++ app/src/main/java/brotifypacha/scheduler/schedules_list_fragment/ScheduleListAdapter.kt
@@ -19,32 +19,36 @@ class ScheduleListAdapter(val itemClickListener: OnScheduleClickListener, val it
     val VIEW_TYPE_HEADER = 0
     val VIEW_TYPE_ITEM = 1
     val VIEW_TYPE_PLUG = 2
+    val VIEW_TYPE_END_SPACING = 3
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         when (viewType){
             VIEW_TYPE_HEADER -> return HeaderViewHolder.from(parent)
             VIEW_TYPE_ITEM -> return ScheduleViewHolder.from(parent)
             VIEW_TYPE_PLUG -> return PlugViewHolder.from(parent)
+            VIEW_TYPE_END_SPACING -> return PlugViewHolder.from(parent)
             else -> throw IllegalArgumentException("Unknown viewType $viewType")
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (getItemViewType(position)){
-            // (position - 2) т.к. первый элемент это заголовок, следовательно все остальные смещены на два
+            // position-2 (0 - header, 1 - plugView which is hidden by default
             VIEW_TYPE_ITEM -> (holder as ScheduleViewHolder).bind(itemClickListener, itemLongClickListener, dataset.get(position - 2))
             VIEW_TYPE_PLUG -> (holder as PlugViewHolder).bind(dataset.isEmpty())
+            VIEW_TYPE_END_SPACING -> (holder as PlugViewHolder).bindEndSpacing()
         }
     }
 
     override fun getItemViewType(position: Int): Int {
         if (position == 0) return VIEW_TYPE_HEADER
         if (position == 1) return VIEW_TYPE_PLUG
+        if (position == dataset.size+2) return VIEW_TYPE_END_SPACING // if it's last item, then it's End Spacing
         return VIEW_TYPE_ITEM
     }
 
     override fun getItemCount(): Int {
-        return dataset.size + 2 // Добавляется место для заголовка
+        return dataset.size + 3 // +1 - header, +1 - plugView, +1 - end spacing
     }
 
     fun setSchedules(schedules: List<Schedule>){
@@ -63,6 +67,15 @@ class ScheduleListAdapter(val itemClickListener: OnScheduleClickListener, val it
                 val v = LayoutInflater.from(parent.context).inflate(R.layout.schedule_list_plug, parent, false)
                 return PlugViewHolder(v, parent)
             }
+        }
+        fun bindEndSpacing(){
+            val tv = TypedValue()
+            var appbarSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56f, parent.context.resources.displayMetrics).toInt()
+            if (parent.context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+                appbarSize = TypedValue.complexToDimensionPixelSize(tv.data, parent.context.getResources().getDisplayMetrics())
+            }
+            v.visibility = View.INVISIBLE
+            v.layoutParams = RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,  (appbarSize * 1.5).toInt())
         }
 
         fun bind(visible: Boolean){
